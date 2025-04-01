d614c009a784837e98290e26f082c5e6066f2d6d
yukino25 <schlosser2018@icloud.com>
2025-03-31 19:31:50 -0700

    commit 1

---
+++ app.js
@@ -0,0 +1,22 @@
+var SerialPort = require('serialport');
+var { ReadlineParser } = require('@serialport/parser-readline')
+const parser = new ReadlineParser({
+    delimiter: '\r\n'
+});
+
+var port = new SerialPort('COM6',{
+    baudRate: 9600,
+    dataBits: 8,
+    parity: 'none',
+    stopBits: 1,
+    flowControl: false,
+});
+
+port.pipe(parser);
+let i = 0;
+setTimeout(function(){
+    while (i < 10000){
+        port.write("2000");
+        i++;
+    }
+}, 3000);
